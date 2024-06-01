c1e0e547f1c8ce278f833e67a4a824cb7c3dea3d
grace ngure <wnguregrace@gmail.com>
2024-06-01 22:25:45 +0300

    feat:lab complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,20 @@
 def write_file(file_name, file_content):
+    file_name = str(file_name) + '.txt'
+    with open(file_name, 'w', encoding='utf-8') as file:
+        file.write(file_content)
+
     pass
 
 def append_file(file_name, append_content):
+    file_name = str(file_name) + '.txt'
+    with open(file_name, 'a', encoding='utf-8') as file:
+        file.write(append_content)
+
+
     pass
 
 def read_file(file_name):
-    pass
+     file_name = str(file_name) + '.txt'
+     with open(file_name, 'r', encoding='utf-8') as file:
+        return file.read()
+     pass
